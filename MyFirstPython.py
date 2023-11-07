48e04d04c4583c02a454ba36c242271a8df06541
veelinktech <104140334+veelinktech@users.noreply.github.com>
2023-11-07 13:41:20 +0530

    New features added_third commit

--- MyFirstPython.py
+++ MyFirstPython.py
@@ -2,5 +2,15 @@ def add(a,b):
   c=a+b
   print("Add result = ", c)
 
+def sub(a,b):
+  c=a-b
+  print("Sub result =", c)
+
+def mul(a,b):
+  c=a*b
+  print("Mul result =",c)
+
 if __name__ == "__main__":
   add()
+  sub()
+  mul()
